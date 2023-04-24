f56a5fc4561375136ef67679953d26f04c071e64
Mohamad-AboAlQuassim <72472724+Mohamad-AboAlQuassim@users.noreply.github.com>
2023-04-24 16:00:42 +0100

    Update streamlit_app.py
    fixed insert error

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,4 +72,4 @@ if streamlit.button('Get Fruit Load List'):
 add_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  streamlit.text(inser_row_snowflake(add_fruit))
+  streamlit.text(insert_row_snowflake(add_fruit))
