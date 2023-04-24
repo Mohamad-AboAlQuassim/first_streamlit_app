1e5ec0d78a541e883fd57b924a5223b707a53b3e
Mohamad-AboAlQuassim <72472724+Mohamad-AboAlQuassim@users.noreply.github.com>
2023-04-24 15:59:32 +0100

    Update streamlit_app.py
    fixed bug where data load did not work from snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ def insert_row_snowflake(new_fruit):
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_row)
+  streamlit.dataframe(my_data_rows)
 
 # add fruit text entry
 add_fruit = streamlit.text_input('What fruit would you like to add?')
