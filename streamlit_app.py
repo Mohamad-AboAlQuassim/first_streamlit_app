0e48e31377d4c1796e66243e4c6984334b490d6b
Mohamad-AboAlQuassim <72472724+Mohamad-AboAlQuassim@users.noreply.github.com>
2023-04-24 09:01:02 +0100

    Update streamlit_app.py
    added pd dataframe for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -7,3 +10,6 @@ streamlit.text('Omega 3 & Blueberry Oatmeal  🥣 ')
 streamlit.text('Kale, Spinach & Rocket Smoothie 🥗')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔')
 streamlit.text('Avocado Toast 🥑🍞')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
