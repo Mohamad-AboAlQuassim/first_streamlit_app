da103455e021630db07295f6c146a21038ab63e1
Mohamad-AboAlQuassim <72472724+Mohamad-AboAlQuassim@users.noreply.github.com>
2023-04-24 09:08:23 +0100

    Update streamlit_app.py
    added prepopulation to multiselect element for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list.set_index('Fruit', inplace=True)
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal  🥣 ')
@@ -15,7 +15,7 @@ streamlit.text('Avocado Toast 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Multi select
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
 
